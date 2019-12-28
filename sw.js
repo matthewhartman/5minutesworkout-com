743dd9cafe7014ba9b1ab0b496cb71b8b7490a30
Matthew Hartman <matthewhartmanau@gmail.com>
2019-12-28 18:14:08 +1100

    Initial commit

---
+++ sw.js
@@ -0,0 +1,64 @@
+// Service Worker
+var CACHE_NAME = 'v-1577449101122'
+var urlsToCache = [
+  '/',
+  '/default.js',
+  '/index.html',
+  '/android-chrome-144x144.png',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+  '/apple-touch-icon.png',
+  '/browserconfig.xml',
+  '/favicon.ico',
+  '/favicon-16x16.png',
+  '/favicon-32x32.png',
+  '/mstile-150x150.png',
+  '/site.webmanifest'
+]
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('Opened cache', CACHE_NAME)
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache')
+        return response
+      }
+      return fetch(event.request).then(function(response) {
+        if(!response || response.status !== 200 || response.type !== 'basic') {
+          return response
+        }
+        var responseToCache = response.clone()
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+          cache.put(event.request, responseToCache)
+        })
+        return response
+      })
+    })
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  var cacheKeeplist = [CACHE_NAME]
+
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          return caches.delete(key)
+        }
+      }))
+    })
+  )
+})
